29e283688d301757c5cd5b4776dbda2388f9d655
nmrika <yryuu@outlook.com>
2020-06-17 16:45:19 +0800

    fuee...

--- app.js
+++ app.js
@@ -84,5 +84,4 @@ client.on('message', (msg) => {
     }
 });
 
-// client.login(process.env.BOT_TOKEN)
-client.login('MzcwOTI4NTI1OTE5NzgwODY2.Xlf5qQ.CB_05Vmvbn2HUbT9NHKC9SfBKWg')
+client.login(process.env.BOT_TOKEN)
