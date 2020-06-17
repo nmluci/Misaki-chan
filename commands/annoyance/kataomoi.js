2faf348b6ded65354b1daa829a3c67aeac1fd55c
nmrika <yryuu@outlook.com>
2020-06-17 16:54:11 +0800

    fuee...

--- commands/annoyance/kataomoi.js
+++ commands/annoyance/kataomoi.js
@@ -13,13 +13,13 @@ module.exports = class KataomoiCommand extends Command {
             group: 'annoyance',
             memberName: 'zuruii',
             description: '片思いは本当に悔しいだよねー',
-            patterns: [/G!|\bdarling\b|\bhoney\b|/i]
+            patterns: [/G!|\bdarling\b|\bhoney\b/gi]
        })
     }
 
     run(msg) {
         const punpun = [`Kyaa?!`, `So Cruel`, `ひどい`, `ズルい`, `もうう`, `スケベ‼`, `ふええ`, `これやっぱ片思いよね～`, `ご主人、あたしが降られた、<${msg.author}>に`]
-        const henji = getRandInt(punpun.length)
+        const henji = punpun[getRandInt(punpun.length)]
 
         msg.say(henji)
         // if (msg.author == 360824982789685248n) msg.say('Fueee');
