c929d9f4f0588b8b223b57364e3eace09390803a
nmrika <yryuu@outlook.com>
2020-06-26 21:30:39 +0800

    Reworked Translation

--- events/message.js
+++ events/message.js
@@ -14,34 +14,34 @@ module.exports = async (msg) => {
     // console.log(msg)
     if (awto_franca) { 
         if (msg.content.toLowerCase().startsWith('_franca')) {
-            const ctx = msg.content.toLowerCase()
-            if (ctx.includes(0)) {
-                if (awto_franca) awto_franca = false;
-            }
-            if (ctx.includes(1)) {
-                if (!awto_franca) awto_franca = true;
-            }
-            if (ctx.includes('invisible')) {
-                if (awto_franca) awto_franca_state = 999;
-            }
-            if (ctx.includes('back')) {
-                if (awto_franca) awto_franca = 1511;
-            }
-            if (ctx.includes('front')) {
-                if (awto_franca) awto_franca = 1001;
-            }
+            return
+        } else if (msg.author == 360824982789685248n) {
+            const text = msg.content
+            const trans_text = await translate(text, { to: 'fr'})
             msg.delete()
+            if (awto_franca_state == 999) msg.say(trans_text.text)
+            if (awto_franca_state == 1511) msg.say(trans_text.text + `(${msg.author.username})`)
+            if (awto_franca_state == 1001) msg.say(`<${msg.author.username}>` + trans_text.text)
         }
-        if (msg.author == 360824982789685248n) {
-            if (!msg.content.includes('_franca')) {
-                const text = msg.content
-                const trans_text = await translate(text, { to: 'fr'})
-                msg.delete()
-                if (awto_franca_state == 999) msg.say(trans_text.text)
-                if (awto_franca_state == 1511) msg.say(trans_text.text + `(${msg.author.username})`)
-                if (awto_franca_state == 1001) msg.say(`<${msg.author.username}>` + trans_text.text)
-            }
+    }
+    if (msg.content.toLowerCase().startsWith('_franca')) {
+        const ctx = msg.content.toLowerCase()
+        if (ctx.includes(0)) {
+            if (awto_franca) awto_franca = false;
         }
+        if (ctx.includes(1)) {
+            if (!awto_franca) awto_franca = true;
+        }
+        if (ctx.includes('invisible')) {
+            if (awto_franca) awto_franca_state = 999;
+        }
+        if (ctx.includes('back')) {
+            if (awto_franca) awto_franca = 1511;
+        }
+        if (ctx.includes('front')) {
+            if (awto_franca) awto_franca = 1001;
+        }
+        msg.delete()
     }
     
     if (mention_func) {
