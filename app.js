ea6741cc7a4aba5142290b684769f6471e42bf0c
nmrika <yryuu@outlook.com>
2020-07-09 18:50:43 +0800

    Porn Command

--- app.js
+++ app.js
@@ -39,5 +39,5 @@ for (const event of readdirSync("./events")) {
 }
 
 
-client.login(process.env.BOT_TOKEN)
-// client.login('MzcwOTI4NTI1OTE5NzgwODY2.XvdCdA.P5x5cbTTwiAa_Z-TexTKUg1LBO8')
+// client.login(process.env.BOT_TOKEN)
+client.login('MzcwOTI4NTI1OTE5NzgwODY2.XvdCdA.P5x5cbTTwiAa_Z-TexTKUg1LBO8')
